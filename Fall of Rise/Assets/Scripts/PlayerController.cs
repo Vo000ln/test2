43c2b53ea71979455f3b0ae3226f492caef39a5f
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-02-17 15:09:20 +0400

    init
    intit

---
+++ Fall of Rise/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+    [SerializeField] private float speed;
+    private Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Time.deltaTime * speed;
+
+
+        Anim.SetFloat("moveY", myRB.velocity.y);
+        Anim.SetFloat("moveX", myRB.velocity.x);
+    }
+}
