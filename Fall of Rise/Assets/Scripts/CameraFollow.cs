c15ccc6fed6552d1ea0ad7e5328139480da92858
albina133 <volkova13albina@gmail.com>
2023-02-17 23:31:51 +0400

    добавила камеру на персонажа

---
+++ Fall of Rise/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    public Vector2 maxPos;
+    public Vector2 minPos;
+
+    private void FixedUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
