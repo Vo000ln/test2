d4d55c2f741fb10f52c781a1ca3a0514d1e02aa1
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-03-21 00:37:42 +0400

    test_vide

---
+++ Fall of Rise/Assets/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VIDE_Data;
+public class UIManager : MonoBehaviour
+{
+    public GameObject container_NPC;
+    public GameObject container_PLAYER;
+    public Text text_NPC;
+    public Text[] text_Choices;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
