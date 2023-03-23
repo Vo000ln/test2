2b2111f1247ff649e9681729fdd73e181d76e91f
albina133 <125047566+albina133@users.noreply.github.com>
2023-03-23 12:12:07 +0400

    поправили скрипт с изменением имени в диалоге

---
+++ Fall of Rise/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VIDE_Data;
+public class PlayerController : MonoBehaviour
+{
+    
+    private Rigidbody2D myRB;
+    [SerializeField] private float speed;
+    private Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        Anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed;
+
+
+        Anim.SetFloat("moveY", myRB.velocity.y);
+        Anim.SetFloat("moveX", myRB.velocity.x);
+        if(Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Vertical") == -1 || Input.GetAxisRaw("Vertical") == 1)
+        {
+            Anim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            Anim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
+    }
+
+}
