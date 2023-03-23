633d24b1977039d448ec0d709adb8678a5b51441
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-03-24 01:22:11 +0400

    добавил передвижение npc(на сцене homeplayer)

--- Fall of Rise/Assets/Scripts/DataClass.cs
+++ Fall of Rise/Assets/Scripts/DataClass.cs
@@ -8,4 +8,10 @@ public static class DataClass
     public static string scene =  "";
     //id - название сцены
     public static string name;
+    //intellect - уровень интеллекта
+    public static int intellect = 0;
+    //brave - смелость
+    public static int brave = 0;
+    //rel_mother - отношение с мамой
+    public static int rel_mother = 0;
 }
--- Fall of Rise/Assets/Scripts/MovedObject.cs
+++ Fall of Rise/Assets/Scripts/MovedObject.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MovedObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Transform transform;
+    public Transform target;
+    public float speed;
+    public bool fl = false;
+    void Start(){
+        transform = GetComponent<Transform>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if(fl){
+            Vector3 tmp = new Vector3(-0.8f,3f,-0.2f) - transform.position;
+            if(tmp.y < 0.05f){
+                transform.position = Vector3.Lerp(transform.position, new Vector3(-0.8f,3f,-0.2f), speed);
+            }
+            else if(tmp.y < 0.6f)
+                transform.position = Vector3.Lerp(transform.position, new Vector3(-0.8f,3f,-0.2f), speed*Time.deltaTime*1.5f);
+            else if(tmp.y < 0.3f)
+                transform.position = Vector3.Lerp(transform.position, new Vector3(-0.8f,3f,-0.2f), speed*Time.deltaTime*3f);    
+            else if(tmp.y < 0.15f)
+                transform.position = Vector3.Lerp(transform.position, new Vector3(-0.8f,3f,-0.2f), speed*Time.deltaTime*6f);    
+            else{
+                transform.position = Vector3.Lerp(transform.position, new Vector3(-0.8f,3f,-0.2f), speed*Time.deltaTime);
+            }
+        }
+    }
+    public void start(){
+        fl = true;
     }
 }
