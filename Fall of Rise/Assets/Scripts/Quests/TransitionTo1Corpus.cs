878a668b2a8d7ca4ef0bac2e5a0553d453ebb1fd
albina133 <125047566+albina133@users.noreply.github.com>
2023-04-17 23:41:15 +0400

    добавила анимацию затемнения в конец сцены HomePlayer + звук на телефон в сцене StidentHostel
    слушаем и кайфуем..

--- Fall of Rise/Assets/Scripts/Quests/TransitionTo1Corpus.cs
+++ Fall of Rise/Assets/Scripts/Quests/TransitionTo1Corpus.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TransitionTo1Corpus : MonoBehaviour
 {
     public bool zone = false;
+    public GameObject fadeIn;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -25,8 +26,16 @@ public class TransitionTo1Corpus : MonoBehaviour
     {
         if (zone && Input.GetKeyDown(KeyCode.E) && DataClass.endDialogue == 1)
         {
-            Application.LoadLevel("StudentHostel");
-            DataClass.endDialogue = 0;
+            fadeIn.SetActive(true); //активируем анимацию
+            StartCoroutine(Coroutine()); //для ожидания и +- нормального перехода сцены
         }
     }
+
+    IEnumerator Coroutine()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        DataClass.endDialogue = 0;
+        Application.LoadLevel("StudentHostel");
+    }
+
 }
