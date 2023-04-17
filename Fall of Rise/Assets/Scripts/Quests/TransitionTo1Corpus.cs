5ae2a4e1a6b3bf4050bc89f4c1e04ca6f562d5c4
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-04-17 15:00:17 +0400

    add chush

---
+++ Fall of Rise/Assets/Scripts/Quests/TransitionTo1Corpus.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionTo1Corpus : MonoBehaviour
+{
+    public bool zone = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            zone = true;
+            //Debug.Log("OBJ");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        zone = false;
+    }
+
+    void Update()
+    {
+        if (zone && Input.GetKeyDown(KeyCode.E))
+        {
+            Application.LoadLevel("Computer");
+        }
+    }
+}
