f3686a2b900ea0d69d4cc743f07fc7664b4ea120
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-05-03 05:22:15 +0400

    tmp

--- Fall of Rise/Assets/Scripts/SpriteSorted.cs
+++ Fall of Rise/Assets/Scripts/SpriteSorted.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpriteSorted : MonoBehaviour
 {
+    private Vector2 size;
+    private Vector2 offset;
     //изменение коллайдеров главного героя если он подошел к нпс, чтобы не наступать на них
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -11,12 +13,16 @@ public class SpriteSorted : MonoBehaviour
         {
             if (other.tag == "PlayerMen")
             {
+                size = new Vector2(0.5625f, 0.2452773f);
+                offset = new Vector2(0, -0.4685132f);  
                 Vector2 S = GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<SpriteRenderer>().sprite.bounds.size;
                 GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().size = S;
                 GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
             }
             if (other.tag == "PlayerWoman")
             {
+                size = new Vector2(0.5625f, 0.2262964f);
+                offset = new Vector2(0, -0.5417128f);  
                 Vector2 S = GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<SpriteRenderer>().sprite.bounds.size;
                 GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().size = S;
                 GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
@@ -26,6 +32,15 @@ public class SpriteSorted : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-
+        if (other.tag == "PlayerMen")
+            {
+                GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().size = size;
+                GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().offset = offset;
+            }
+            if (other.tag == "PlayerWoman")
+            {
+                GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().size = size;
+                GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().offset = offset;
+            }
     }
 }
