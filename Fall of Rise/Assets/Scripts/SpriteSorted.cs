8cce2abc815da718c729777fd3fe62d4ec554fc9
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-05-03 07:11:34 +0400

    добавил коллайдеры(условные) на нпсшников

--- Fall of Rise/Assets/Scripts/SpriteSorted.cs
+++ Fall of Rise/Assets/Scripts/SpriteSorted.cs
@@ -4,48 +4,10 @@ using UnityEngine;
 
 public class SpriteSorted : MonoBehaviour
 {
-    private Vector2 size;
-    private Vector2 offset;
+    // private Vector2 size;
+    // private Vector2 offset;
+    private Transform pos;
     //изменение коллайдеров главного героя если он подошел к нпс, чтобы не наступать на них
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "PlayerMen" || other.tag == "PlayerWoman")
-        {
-            if (other.tag == "PlayerMen")
-            {
-                size = new Vector2(0.5625f, 0.2452773f);
-                offset = new Vector2(0, -0.4685132f);  
-                Vector2 S = GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<SpriteRenderer>().sprite.bounds.size;
-                GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().size = S;
-                GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
-            }
-            if (other.tag == "PlayerWoman")
-            {
-                size = new Vector2(0.5625f, 0.2262964f);
-                offset = new Vector2(0, -0.5417128f);  
-                Vector2 S = GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<SpriteRenderer>().sprite.bounds.size;
-                GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().size = S;
-                GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "PlayerMen")
-            {
-                GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().size = size;
-                GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().offset = offset;
-            }
-            if (other.tag == "PlayerWoman")
-            {
-                GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().size = size;
-                GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().offset = offset;
-            }
-    }
-    // void Start(){
-    //     pos = GetComponent<Transform>();
-    // }
     // private void OnTriggerEnter2D(Collider2D other)
     // {
     //     if (other.tag == "PlayerMen" || other.tag == "PlayerWoman")
@@ -54,13 +16,17 @@ public class SpriteSorted : MonoBehaviour
     //         {
     //             size = new Vector2(0.5625f, 0.2452773f);
     //             offset = new Vector2(0, -0.4685132f);  
-    //             pos.position = new Vector3(pos.position.x, pos.position.y, -1f);
+    //             Vector2 S = GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<SpriteRenderer>().sprite.bounds.size;
+    //             GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().size = S;
+    //             GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
     //         }
     //         if (other.tag == "PlayerWoman")
     //         {
     //             size = new Vector2(0.5625f, 0.2262964f);
-    //             offset = new Vector2(0, -0.5417128f);     
-    //             pos.position = new Vector3(pos.position.x, pos.position.y, -1f);
+    //             offset = new Vector2(0, -0.5417128f);  
+    //             Vector2 S = GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<SpriteRenderer>().sprite.bounds.size;
+    //             GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().size = S;
+    //             GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().offset = new Vector2(0, 0);
     //         }
     //     }
     // }
@@ -69,11 +35,33 @@ public class SpriteSorted : MonoBehaviour
     // {
     //     if (other.tag == "PlayerMen")
     //         {
-    //             pos.position = new Vector3(pos.position.x, pos.position.y, -2f);
+    //             GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().size = size;
+    //             GameObject.FindGameObjectWithTag("PlayerMen").GetComponent<BoxCollider2D>().offset = offset;
     //         }
     //         if (other.tag == "PlayerWoman")
     //         {
-    //             pos.position = new Vector3(pos.position.x, pos.position.y, -2f);
+    //             GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().size = size;
+    //             GameObject.FindGameObjectWithTag("PlayerWoman").GetComponent<BoxCollider2D>().offset = offset;
     //         }
     // }
+    void Start(){
+        pos =  GetComponent<Transform>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "PlayerMen" || other.tag == "PlayerWoman")
+        {
+            pos.position = new Vector3(pos.position.x, pos.position.y, -0.6f);
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "PlayerMen" || other.tag == "PlayerWoman")
+        {
+            pos.position = new Vector3(pos.position.x, pos.position.y, -2.4f);
+            
+        }
+    }
 }
