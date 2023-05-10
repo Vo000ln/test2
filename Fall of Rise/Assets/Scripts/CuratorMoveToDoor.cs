ff4b4ccc103492bbda24485762a715a6a9261157
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-05-10 23:25:59 +0400

    куратор в дверь идет не хило

---
+++ Fall of Rise/Assets/Scripts/CuratorMoveToDoor.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class CuratorMoveToDoor : MonoBehaviour
+{
+    public Transform transform;
+    public float speed;
+    public bool fl = false;
+    public bool next = false;
+    void Start(){
+        transform = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        if(fl)
+        {
+            Vector3 tmp = new Vector3(16f, -2.6f, -2f) - transform.position;
+            if(!next){
+                if(tmp.y < 0.05f){
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(16f, -2.6f, -2f), speed*Time.deltaTime*6f);
+                }
+                if(tmp.y < 0.01f){
+                    next = true;
+                }
+                else if(tmp.y < 0.6f)
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(16f, -2.6f, -2f), speed*Time.deltaTime*2f);
+                else if(tmp.y < 0.3f)
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(16f, -2.6f, -2f), speed*Time.deltaTime*3f);    
+                else if(tmp.y < 0.15f)
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(16f, -2.6f, -2f), speed*Time.deltaTime*4f);    
+                else{
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(16f, -2.6f, -2f), speed*Time.deltaTime);
+                }
+            }
+            if(next){
+                transform.position = Vector3.Lerp(transform.position, new Vector3(16f, -2f, -2f), speed*Time.deltaTime*3f);
+            }
+            //next = true;
+        }
+        if(next){
+            transform.position = Vector3.Lerp(transform.position, new Vector3(16f, -2f, -2f), speed*Time.deltaTime);
+        }
+    }
+    public void start(){
+        fl = true;
+        //DataClass.choose_conversation_mommy = 3; //������������ ������ ����� ������, ����� ���� ���� ���������� ������
+        //GetComponent<Animator>().enabled = true;
+        //StartCoroutine(Coroutine());
+ 
+    }
+ 
+    IEnumerator Coroutine()
+    {
+        yield return new WaitForSecondsRealtime(3);
+        GetComponent<Animator>().enabled = false;
+    }
+}
