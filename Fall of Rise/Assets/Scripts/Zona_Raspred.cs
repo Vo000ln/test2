5015d0d2adbd97acca4811e6ee078d0bc2d40086
Vo000ln <91181230+Vo000ln@users.noreply.github.com>
2023-05-28 23:11:06 +0400

    фигняяяяяя

---
+++ Fall of Rise/Assets/Scripts/Zona_Raspred.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Zona_Raspred : MonoBehaviour
+{
+    public GameObject canvas_1, canvas_2, canvas_3, canvas_4, canvas_5, canvas_6, canvas_7, canvas_8, canvas_9, canvas_10, canvas_11, canvas_12;
+
+}
